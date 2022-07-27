5376ab5f15ede14097802e37cd2863e50e215543
S. Capodicci <scapodicci@MacBook-Pro.local>
2022-07-27 18:14:37 -0400

    About Me and npm install

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,30 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import sam_profile_pic from "../assets/sam_profile_pic.jpeg";
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
         <div class="split left">
           <div className="centered">
-            <img
+            <img 
               className="profile_image"
-              src={profile_pic}
+              src={sam_profile_pic}
               alt="Profile Pic"
-            ></img>
+              ></img>
           </div>
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Your Name</div>
+            <div className="name_title">Sam Capodicci</div>
             <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+            <p>Computer science and programming are what I love to do. My favorite way to get practice is by writing software for my friends' passion projects! Ask me about my Unity games or fleet of Discord bots.</p>
+            <p>I also enjoy the arts. I am currently Assistant Director for a theater program at my school, University of Notre Dame, and I have been a lead actor and finalist in speech contests. I am a hobbyist digital artist and occasionally sell my work online!</p>
             </div>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
